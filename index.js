825a5d1f7fb9281c2542ef72f31333bfda46d81b
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-11-08 23:07:27 +0600

    final touch

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors({
-  origin:[
-    'http://localhost:5173'
+  origin:['http://localhost:5173',
+    'https://voyagelodge.web.app', 'https://voyagelodge.firebaseapp.com'
   ],
   credentials: true
 }))
