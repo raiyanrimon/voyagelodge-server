83d59669ed116dd03fe972b93be9f30e6a39855a
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-11-08 21:29:54 +0600

    vercel config added

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors({
   credentials: true
 }))
 app.use(express.json())
+app.use(cookieParser())
 
 
 
@@ -30,6 +31,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const logger = (req, res, next)=>{
+  console.log('log info', req.method, req.url);
+  next()
+}
+const verifyToken = (req, res, next) =>{
+  const token = req?.cookies?.token
+  console.log('token in middle', token);
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  jwt.verify(token , process.env.ACCESS_SECRET_TOKEN, (err, decoded)=>{
+    if (err){
+      return res.status(401).send({message: 'unauthorized access'})
+
+    }
+    req.user = decoded
+    next()
+  })
+ 
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,7 +62,8 @@ async function run() {
     const bookingsCollection = client.db('voyagelodge').collection('bookings')
     const reviewsCollection = client.db('voyagelodge').collection('reviews')
 
-    app.post('/jwt', async (req,res)=>{
+    //  auth related api
+    app.post('/jwt',logger, async (req,res)=>{
       const user = req.body
       console.log('user for token', user);
       const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN , {expiresIn: '1h'})
@@ -66,12 +91,26 @@ async function run() {
         res.send(result)
     })
     app.post('/bookings', async(req,res)=>{
+
         const bookings = req.body
         const result = await bookingsCollection.insertOne(bookings)
         res.send(result)
     })
-    app.get('/bookings', async(req,res)=>{
-        const result = await bookingsCollection.find().toArray()
+    app.get('/room-available', async (req, res)=>{
+      const result = await bookingsCollection.find().toArray()
+        res.send(result)
+    })
+    app.get('/bookings', logger, verifyToken, async(req,res)=>{
+      console.log(req.query.email)
+      console.log('cook',req.user)
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      let query = {}
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+        const result = await bookingsCollection.find(query).toArray()
         res.send(result)
     })
     app.delete('/bookings/:id', async(req, res)=>{
